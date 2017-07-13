ed2bdf5f418a83d3370d7bfe6d5ab79e4ee65673
Simon Deshaies <pre63@users.noreply.github.com>
2017-07-13 14:41:00 -0400

    replaced splice with slice (#28)

--- src/compose/index.js
+++ src/compose/index.js
@@ -1,5 +1,5 @@
 // compose :: ... -> (Any -> Any)
 export default (...rest) => (...a) =>
   rest
-    .splice(0, rest.length - 1)
-    .reduceRight((acc, func) => func(acc), rest.splice(rest.length - 1)[0](...a))
+    .slice(0, rest.length - 1)
+    .reduceRight((acc, func) => func(acc), rest.slice(rest.length - 1)[0](...a))
